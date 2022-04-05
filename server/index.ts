b398f3dff00d9bf19a4643cb39b0149dc23ea835
Alex Bal <alexenri12@gmail.com>
2022-04-05 18:23:30 -0300

    mejora

--- server/index.ts
+++ server/index.ts
@@ -80,8 +80,9 @@ const staticDirPath = path.resolve(__dirname, "../client");
     const mascotasCercanas = await nearbyPets(req.query.lng, req.query.lat);
     res.json(mascotasCercanas);
   });
-  app.patch("/pets", async (req, res) => {
-    const respuesta = await updatePet(req.body.id, req.body);
+  app.patch("/pets/:petId", async (req, res) => {
+    const { petId } = req.params;
+    const respuesta = await updatePet(petId, req.body);
     res.json(respuesta);
   });
 
